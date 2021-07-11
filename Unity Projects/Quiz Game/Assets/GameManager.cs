ca2f436cbd20bf12b03e161dc92745af8750ff20
unknown <tuberlinhoa@gmail.com>
2021-07-11 20:06:07 +0200

    neue Version

---
+++ Unity Projects/Quiz Game/Assets/GameManager.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+public Question[] questions;
+private static List<Question> unansweredQuestions;
+
+private Question currentQuestion;
+
+[SerializeField]
+private Text factText;
+
+[SerializeField]
+private Text trueAnswerText;
+
+[SerializeField]
+private Text falseAnswerText;
+
+[SerializeField]
+private float timeBetweenQuestions = 1f;
+
+[SerializeField]
+private Animator animator;
+
+void Start () 
+{
+
+    if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+    {
+      unansweredQuestions= questions.ToList<Question>();
+    }
+
+     SetCurrentQuestion();
+  }
+
+  void SetCurrentQuestion()
+  {
+
+    int randomQuestionIndex = Random.Range(0,unansweredQuestions.Count);
+    currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+    factText.text = currentQuestion.fact;
+
+    if(currentQuestion.isTrue)
+    {
+      trueAnswerText.text="CORRECT";
+      falseAnswerText.text = "WRONG";
+
+    }else
+    {
+      trueAnswerText.text = "WRONG";
+      falseAnswerText.text = "CORRECT";
+    }
+
+  }
+
+  IEnumerator TransitionToNextQuestion()
+  {
+    unansweredQuestions.Remove(currentQuestion);
+    
+    yield return new WaitForSeconds(timeBetweenQuestions);
+
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+  }
+
+  
+  public void UserSelectTrue()
+  {
+    animator.SetTrigger("True");
+    if(currentQuestion.isTrue)
+    {
+      Debug.Log("Correct!");
+
+    } else
+    {
+      Debug.Log("Wrong");
+    }
+
+    StartCoroutine(TransitionToNextQuestion());
+
+  }
+
+  
+  public void UserSelectFalse()
+  {
+    animator.SetTrigger("False");
+    if(!currentQuestion.isTrue)
+    {
+      Debug.Log("Correct!");
+
+    } else
+    {
+      Debug.Log("Wrong");
+    }
+
+     StartCoroutine(TransitionToNextQuestion());
+
+  }
+
+}
---
+++ Unity Projects/Quiz Game/Assets/Question.cs
@@ -0,0 +1,7 @@
+
+[System.Serializable] //notify unity and the system that this class can store information
+
+public class Question {
+    public string fact;
+    public bool isTrue;
+}
