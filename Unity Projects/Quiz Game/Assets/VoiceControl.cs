5506776fcc539173225d87d4c1177da064fddf66
Vincent Clemens Kelbel <vincent.c.kelbel@campus.tu-berlin.de>
2021-07-11 18:52:26 +0200

    Speech Reconigtion

---
+++ Unity Projects/Quiz Game/Assets/VoiceControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    void Start()
+    {
+        actions.Add('True', TrueAction);
+        actions.Add('False', FalseAction);
+
+        keywordRecognizer = new KeywordRecognizer(replys.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraeRecognizedEventArgs speech){
+        Debug.log(speech.text);
+
+        actions[speech.text].Invoke();
+
+    }
+
+    private void TrueAction(){
+        //TODO: Call Function that is called when True Button is pressed;
+    }
+
+    private void FalseAction(){
+        //TODO: Call Function that is called when False Button is pressed;
+    }
+
+}
